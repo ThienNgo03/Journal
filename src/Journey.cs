263450a76334ebadf6f3beeb8747ffc2b19a3629
unknown <ngongocthien56@gmail.com>
2025-07-11 21:32:11 +0700

    Location

--- src/Journey.cs
+++ src/Journey.cs
@@ -7,6 +7,8 @@
 
         public string Content { get; set; } = string.Empty; // cột nội dung nhật ký
 
+        public string? Location { get; set; }
+
         public DateTime Date { get; set; } // cột ngày viết nhật ký này
     }
 }
