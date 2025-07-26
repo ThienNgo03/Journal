1197b789dfef9bde7db3f5a40e699030cc69ffc8
unknown <ngongocthien56@gmail.com>
2025-07-26 21:55:31 +0700

    ver1

--- src/WeekPlans/Update/Payload.cs
+++ src/WeekPlans/Update/Payload.cs
@@ -6,13 +6,15 @@
 
         public Guid WorkoutId { get; set; }
 
-        public DateTime DateOfWeek { get; set; }
+        public string DateOfWeek { get; set; }
+
+        //public DayOfWeek DateOfWeek { get; set; }
 
         public DateTime Time { get; set; }
 
         public int Rep { get; set; }
 
-        public int HoldingTime { get; set; }
+        public TimeSpan HoldingTime { get; set; }
 
         public int Set { get; set; }
 
